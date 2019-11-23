8a88983ffbddbe9ebe4a12e68a4c48ffc2957fbe
Pierre Monvoisin <monvoisin.pierre@gmail.com>
2019-11-23 11:26:03 +0100

    Ajout de la nav barre et création des nouvelles pages

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+$(function(){
+  var mouseX, mouseY;
+
+  $(document).mousemove(function(e) {
+    mouseX = e.pageX;
+    mouseY = e.pageY;
+    console.log('mouseY = ' + mouseY)
+    if ( mouseY < 2) {
+      $('header').slideDown('fast');
+    }
+    if ( mouseY > $('header').height()) {
+      $('header').slideUp('slow');
+    }
+  })
+});
